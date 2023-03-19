0f1a168e016ca8c481fc23c85894e6872296fab2
unknown <bosamia.karan@gmail.com>
2023-03-20 01:39:49 +0530

    [ADD] Adds webhook to fetch followed data and in preparation to use post request to add follow request

--- models/blog_users.py
+++ models/blog_users.py
@@ -14,6 +14,13 @@ class BlogUsers(db.Model):
     name = db.Column(db.String(256))
     password = db.Column(db.String(256))
     profile_picture = db.Column(db.LargeBinary, nullable = True)
+    followed_ids = db.relationship(
+        "Follow", back_populates="followed", foreign_keys='Follow.followed_id'
+    )
+    follower_ids = db.relationship(
+        "Follow", back_populates="follower", foreign_keys='Follow.follower_id'
+    )
+
 
 class BlogPost(db.Model):
     __tablename__ = 'blog_post'
@@ -22,3 +29,16 @@ class BlogPost(db.Model):
     name = db.Column(db.String(80), nullable=False)
     caption = db.Column(db.String(250))
     image = db.Column(db.LargeBinary, nullable = True)
+
+
+class Follow(db.Model):
+    __tablename__ = "follows"
+
+    id = db.Column(db.Integer, primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey("blog_users.id"))
+    follower_id = db.Column(db.Integer, db.ForeignKey("blog_users.id"))
+    # making sure (followed_id, follower_id) are unique
+    db.UniqueConstraint(followed_id, follower_id)
+
+    follower = db.relationship("BlogUsers", foreign_keys=[follower_id])
+    followed = db.relationship("BlogUsers", foreign_keys=[followed_id])
