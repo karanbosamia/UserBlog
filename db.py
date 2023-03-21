edd2fa73f4788f42dd0a7e1c2d32cc59fb8253a2
unknown <bosamia.karan@gmail.com>
2023-03-22 02:01:13 +0530

    [ADD] Adds the logic to follow and unfollow the user from search view

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Secret@@###!!@@(*@Karan'
-CORS(app, support_credentials=True)
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:8081"}}, supports_credentials=True)
 DB_URL = "sqlite:///userblog.sqlite3"
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
